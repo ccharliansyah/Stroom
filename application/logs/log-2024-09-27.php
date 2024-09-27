09ff80c77f8bcb489271295654303ac43a4ab5e6
ccharliansyah <ccharliansyah@gmail.com>
2024-09-27 15:42:18 +0700

    perbaikan url verif 6

---
+++ application/logs/log-2024-09-27.php
@@ -0,0 +1,40 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2024-09-27 15:41:58 --> Config Class Initialized
+INFO - 2024-09-27 15:41:58 --> Hooks Class Initialized
+DEBUG - 2024-09-27 15:41:58 --> UTF-8 Support Enabled
+INFO - 2024-09-27 15:41:58 --> Utf8 Class Initialized
+INFO - 2024-09-27 15:41:58 --> URI Class Initialized
+DEBUG - 2024-09-27 15:41:58 --> No URI present. Default controller set.
+INFO - 2024-09-27 15:41:58 --> Router Class Initialized
+INFO - 2024-09-27 15:41:58 --> Output Class Initialized
+INFO - 2024-09-27 15:41:58 --> Security Class Initialized
+DEBUG - 2024-09-27 15:41:58 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-09-27 15:41:58 --> Input Class Initialized
+INFO - 2024-09-27 15:41:58 --> Language Class Initialized
+INFO - 2024-09-27 15:41:58 --> Loader Class Initialized
+INFO - 2024-09-27 15:41:58 --> Helper loaded: url_helper
+INFO - 2024-09-27 15:41:58 --> Helper loaded: form_helper
+DEBUG - 2024-09-27 15:41:58 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2024-09-27 15:41:58 --> Session: Class initialized using 'files' driver.
+INFO - 2024-09-27 15:41:58 --> Controller Class Initialized
+INFO - 2024-09-27 15:41:58 --> Config Class Initialized
+INFO - 2024-09-27 15:41:58 --> Hooks Class Initialized
+DEBUG - 2024-09-27 15:41:58 --> UTF-8 Support Enabled
+INFO - 2024-09-27 15:41:58 --> Utf8 Class Initialized
+INFO - 2024-09-27 15:41:58 --> URI Class Initialized
+INFO - 2024-09-27 15:41:58 --> Router Class Initialized
+INFO - 2024-09-27 15:41:58 --> Output Class Initialized
+INFO - 2024-09-27 15:41:58 --> Security Class Initialized
+DEBUG - 2024-09-27 15:41:58 --> Global POST, GET and COOKIE data sanitized
+INFO - 2024-09-27 15:41:58 --> Input Class Initialized
+INFO - 2024-09-27 15:41:58 --> Language Class Initialized
+INFO - 2024-09-27 15:41:58 --> Loader Class Initialized
+INFO - 2024-09-27 15:41:58 --> Helper loaded: url_helper
+INFO - 2024-09-27 15:41:58 --> Helper loaded: form_helper
+DEBUG - 2024-09-27 15:41:58 --> Session: "sess_save_path" is empty; using "session.save_path" value from php.ini.
+INFO - 2024-09-27 15:41:58 --> Session: Class initialized using 'files' driver.
+INFO - 2024-09-27 15:41:58 --> Controller Class Initialized
+INFO - 2024-09-27 15:41:58 --> File loaded: D:\xampp\htdocs\stroom\application\views\dashboard.php
+INFO - 2024-09-27 15:41:58 --> Final output sent to browser
+DEBUG - 2024-09-27 15:41:58 --> Total execution time: 0.1300
